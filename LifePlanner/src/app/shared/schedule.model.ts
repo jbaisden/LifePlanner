4e39b34642281c63b84bf6663eee8e62608943d8
jbaisden <jasonbaisden@gmail.com>
2019-02-12 14:11:03 -0600

    day of week and time

--- LifePlanner/src/app/shared/schedule.model.ts
+++ LifePlanner/src/app/shared/schedule.model.ts
@@ -3,7 +3,7 @@ import { Appointment } from "src/app/shared/appointment.model";
 
 export class Schedule {
 
-    public professionalId:number;
+    public professionalId: number;
     public scheduleId: number;
     public clientId: number;
     public appointments: Appointment[];
@@ -13,10 +13,33 @@ export class Schedule {
         this.clientId = clientId;
         this.appointments = [];
     }
-
 }
 
 export class DayOfWeekAndTime {
     public dayOfWeek: string;
     public time: Time;
+
+    public getSortId(): number {
+        var sortIdvalue: number;
+        switch (this.dayOfWeek) {
+            case "Monday":
+                sortIdvalue = 1; break;
+            case "Tuesday":
+                sortIdvalue = 2; break;
+            case "Wednesday":
+                sortIdvalue = 3; break;
+            case "Thursday":
+                sortIdvalue = 4; break;
+            case "Friday":
+                sortIdvalue = 5; break;
+            case "Saturday":
+                sortIdvalue = 6; break;
+            case "Sunday":
+                sortIdvalue = 7; break;
+            default:
+                sortIdvalue = 8; break;
+        }
+
+        return sortIdvalue;
+    }
 }
